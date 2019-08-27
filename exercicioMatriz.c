ff057296f949aa213699d03c75551f4342f073f4
Thiago Silva <39065653+thdsilva@users.noreply.github.com>
2019-08-27 17:55:17 -0300

    Arquivos upados

---
+++ exercicioMatriz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+	int matrizA [3][4];
+	int matrizB [3][4];
+	//int i = 0;
+	
+	printf("--- Iforme os Dados da Matriz A ---\n");
+	for(int i = 0; i <3; i++){
+		for(int j = 0; j <4; j++){
+			printf("\nNumero[%d][%d]:", i+1, j+1);
+			scanf("%i", &matrizA[i][j]);
+		}
+	}
+	
+		printf("--- Iforme os Dados da Matriz B ---\n");
+	for(int i = 0; i <3; i++){
+		for(int j = 0; j <4; j++){
+			printf("\nNumero[%d][%d]:", i+1, j+1);
+			scanf("%i", &matrizB[i][j]);
+		}
+	}
+
+	printf("--- Exibindo os dados da matriz A*B ---");
+	for(int i = 0; i <3; i++){
+		for(int j = 0; j <4; j++){
+			printf("\nNumero[%d][%d]:", i+1, j+1);
+			printf("%i", matrizA[i][j] * matrizB[i][j]);
+		}
+	}
+}
